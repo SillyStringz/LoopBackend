ed1e2f201c4bb9934a9d552537cbd1308dee4012
c-mac88 <christianmcfarland@gmail.com>
2016-07-31 03:07:03 -0700

    Parse returned undefined in my last test. Added a 'get file' configuration line to index.js to get the say_hello file to see if it now recognizes Parse

--- index.js
+++ index.js
@@ -8,34 +8,35 @@ var ParseDashboard = require('parse-dashboard');
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 
 if (!databaseUri) {
-  console.log('DATABASE_URI not specified, falling back to localhost.');
+    console.log('DATABASE_URI not specified, falling back to localhost.');
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
-  cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || '2cc7c323bdc48c1765d8d8d05e27d720',
-  masterKey: process.env.MASTER_KEY || '51209eac0010a40227c66a9feabab99d', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://cryptic-escarpment-48353.herokuapp.com/parse',  // Don't forget to change to https if needed
-  liveQuery: {
-    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  }
+    databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+    cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
+    cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
+    appId: process.env.APP_ID || '2cc7c323bdc48c1765d8d8d05e27d720',
+    masterKey: process.env.MASTER_KEY || '51209eac0010a40227c66a9feabab99d', //Add your master key here. Keep it secret!
+    serverURL: process.env.SERVER_URL || 'http://cryptic-escarpment-48353.herokuapp.com/parse', // Don't forget to change to https if needed
+    liveQuery: {
+        classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
+    }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
 // javascriptKey, restAPIKey, dotNetKey, clientKey
 
 var dashboard = new ParseDashboard({
-  "apps": [{
-    "serverURL": process.env.SERVER_URL || 'http://cryptic-escarpment-48353.herokuapp.com/parse',
-    "appId": process.env.APP_ID || '2cc7c323bdc48c1765d8d8d05e27d720',
-    "masterKey": process.env.MASTER_KEY || '51209eac0010a40227c66a9feabab99d',
-    "appName": 'Loop'
-  }],
-  "users": [{
-    "user": "loop",
-    "pass": "pass123"
-  }]
+    "apps": [{
+        "serverURL": process.env.SERVER_URL || 'http://cryptic-escarpment-48353.herokuapp.com/parse',
+        "appId": process.env.APP_ID || '2cc7c323bdc48c1765d8d8d05e27d720',
+        "masterKey": process.env.MASTER_KEY || '51209eac0010a40227c66a9feabab99d',
+        "appName": 'Loop'
+    }],
+    "users": [{
+        "user": "loop",
+        "pass": "pass123"
+    }]
 }, true);
 
 
@@ -50,13 +51,13 @@ app.use(mountPath, api);
 
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function(req, res) {
-  res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
+    res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
 });
 
 // There will be a test page available on the /test path of your server url
 // Remove this before launching your app
-app.get('/test', function(req, res) {
-  res.sendFile(path.join(__dirname, '/public/test.html'));
+app.get('/bin/say_hello', function(req, res) {
+    res.sendFile(path.join(__dirname, '/bin/say_hello'));
 });
 
 var port = process.env.PORT || 1337;
